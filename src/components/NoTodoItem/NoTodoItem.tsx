36322440d1cba5e06eb3cd33d04d9a66261ffa50
Juan Gustin <juandgustin@gmail.com>
2023-08-15 15:41:57 -0500

    Filters & Searchbar (Mixed): Working - V3

--- src/components/NoTodoItem/NoTodoItem.tsx
+++ src/components/NoTodoItem/NoTodoItem.tsx
@@ -1,12 +1,10 @@
 const NoTodoItem = () => {
   return (
-    <div className='card w-80 h-80 border-2 border-black p-5'>
-      <h1 className='text-xl'>
-        No tienes tareas agregadas (con estas características).
-      </h1>
-      <hr />
-      <h1 className='text-xl'>
-        Intenta añadiendo tareas o borrando los filtros de búsqueda.
+    <div className='card w-[600px] h-40 border-2 border-black p-5 flex flex-col justify-between items-center'>
+      <h1 className='text-xl'>¡Oh no! No tienes tareas agregadas.</h1>
+      <hr className='bg-black' />
+      <h1 className='text-lg'>
+        TIP: Intenta añadiendo tareas o borrando los filtros de búsqueda.
       </h1>
     </div>
   );
